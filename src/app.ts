61cb621d980024cf4f77fcb59e52c66f002d2c51
Sholaropo <ropoolusola@gmail.com>
2025-01-31 22:26:17 +0100

    feat: added server health test

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,21 @@
-const morgan = require("morgan");
-const express = require('express')
+import morgan from "morgan";
+import express from "express";
+
 const port = 3000;
 
-const app = express()
-app.use(express.json())
+const app = express();
+
+app.use(express.json());
 app.use(morgan("combined"));
 
-app.get("/", (req, res) => {
-  res.send("Hello, Express!");
+app.get("/health", (req, res) => {
+  res.status(200).send("Server is healthy");
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+  });
+}
+
+export default app;
