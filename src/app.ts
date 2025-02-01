3d72d8f21c1b315fe485a0abb5217c26d6376649
Sholaropo <ropoolusola@gmail.com>
2025-02-01 18:36:25 +0100

    feat: swagger config

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 import morgan from "morgan";
 import express from "express";
+import swaggerUi from 'swagger-ui-express';
+import swaggerSpec from './config/swagger.js';
 
 const port = process.env.PORT || 3000;
 
@@ -7,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use(morgan("combined"));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get("/health", (req, res) => {
   res.status(200).send("Server is healthy");
